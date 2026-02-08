a1cd7a6261366bd9d7fbc83e5de9d6088cbc6523
cryptobox <pray.happy76@gmail.com>
2026-02-08 10:14:59 +0900

    fix: delete comment

--- src/app/Home.jsx
+++ src/app/Home.jsx
@@ -9,12 +9,6 @@ function Lesson({ item, completeAction }) {
   }
   return (
     <Design.LessonCard item={item}>
-      {/* 
-          Design.CompleteButton is using the action prop pattern to automatically
-          update the completed state while the action is pending. If the action to
-          toggle complete takes longer than 150ms, it automatically shows a loading
-          state on the button, so the user knows their action is being processed.
-      */}
       <Design.CompleteButton
         complete={item.complete}
         action={action}
@@ -64,53 +58,20 @@ export default function Home() {
   const tab = router.search.tab || "all";
 
   function searchAction(value) {
-    /**
-     * Since this is an Action we know this updates in a transition.
-     */
     router.setParams("q", value);
   }
+
   function tabAction(value) {
-    /**
-     * Since this is an Action we know this updates in a transition.
-     */
     router.setParams("tab", value);
   }
 
   async function completeAction(id) {
-    /**
-     * Since we're in an Action we know we're in a transition.
-     * This means we can await a mutation, and the pending state of
-     * the action will be true until the mutation, and all the updates
-     * after it are done.
-     */
     await data.mutateToggle(id);
-
-    /**
-     * After the mutation we need to revalidate the data cache.
-     * In this example app, our router and data layer are integrated,
-     * so when you call `refresh` on the current route, the data cache
-     * is automatically cleared so re-rendering the route re-fetches data.
-     *
-     * Note: We don't have to wrap this in startTransition because
-     * the router wraps these updates in a transition automatically.
-     */
     router.refresh();
   }
   return (
     <>
-      {/*
-         Design.SearchInput is using the action prop pattern to automatically 
-         show a loading state while the action is pending (delayed by 1.5s).
-         The input state is updated with useOptimistic so it updates immediately
-         while the transition to the new URL is pending.
-      */}
       <Design.SearchInput value={search} changeAction={searchAction} />
-      {/*
-         Design.TabList is using the action prop pattern to optimistically 
-         update the selected tab while the action is pending. If the action 
-         takes longer than 150ms, it automatically shows a loading state on
-         the tab, so the user knows their optimistic tab is still loading.
-      */}
       <Design.TabList activeTab={tab} changeAction={tabAction}>
         <LessonList
           tab={tab}
